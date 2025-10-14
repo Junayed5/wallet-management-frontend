2450b75211eb0a1bcae69fa02c606f8b567af3ee
Junayed5 <mdjunayed352003@gmail.com>
2025-10-14 08:27:07 +0600

    Depley error solve

--- src/pages/User/UserTransaction.tsx
+++ src/pages/User/UserTransaction.tsx
@@ -13,6 +13,7 @@ const UserTransaction = () => {
     userNumber: data?.wallet?.phone,
     params: { page },
   });
+  console.log(userTransactions)
 
   if (isLoading) {
     toast.loading("Loading transactions...", { id: "load-transaction" });
@@ -21,20 +22,28 @@ const UserTransaction = () => {
   }
 
   return (
-    <div className="container mx-auto">
-      <div className="mb-4 flex items-center justify-between">
-        <h1 className="text-2xl font-semibold text-primary">Transactions</h1>
-        {/* <FilterDropdown /> */}
+    <>
+    {userTransactions && userTransactions?.transactions?.length > 0 ? (
+      <div className="container mx-auto">
+        <div className="mb-4 flex items-center justify-between">
+          <h1 className="text-2xl font-semibold text-primary">Transactions</h1>
+          {/* <FilterDropdown /> */}
+        </div>
+        <div className="my-4">
+          <TransactionTable data={userTransactions} />
+        </div>
+        <PaginationSection
+          currentPage={userTransactions?.pagination?.page}
+          setCurrentPage={setCurrentPage}
+          totalPages={userTransactions?.pagination?.totalPages}
+        />
       </div>
-      <div className="my-4">
-        <TransactionTable data={userTransactions} />
+    ) : (
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-semibold text-primary text-center">No Transactions Found</h1>
       </div>
-      <PaginationSection
-        currentPage={userTransactions?.pagination?.page}
-        setCurrentPage={setCurrentPage}
-        totalPages={userTransactions?.pagination?.totalPages}
-      />
-    </div>
+    )}
+    </>
   );
 };
 
